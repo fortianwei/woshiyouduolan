d13ca10409a65c30ba468af5376323d382d2c87a
tianwei <fortianwei@163.com>
2015-01-01 11:55:24 +0800

    add post

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,5 +1,5 @@
-__author__ = 'tianwei'
 from tornado.web import RequestHandler
+from datetime import datetime
 
 class BaseHandler(RequestHandler):
 
@@ -13,6 +13,19 @@ class WelcomeHandler(BaseHandler):
         print 1111
         self.render('index.html')
 
+
 class PostHandler(BaseHandler):
     def get(self):
         self.render('post.html')
+
+    def post(self, *args, **kwargs):
+        title = self.get_body_argument('title', default='No title')
+        content = self.get_body_argument('content', default='No content')
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        articles = self.db.articles
+
+        article ={}
+
+        print self.get_body_argument('title')
+        print self.get_body_argument('content')
+        print time.strftime('%Y-%m-%d %H:%M:%S')
