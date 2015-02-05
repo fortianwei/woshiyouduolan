9bfba1ccb978532d14383692fec64f5a29dc7172
tianwei <fortianwei@gmail.com>
2015-02-05 15:59:43 +0800

    articles post recently being limited to 10

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -29,7 +29,7 @@ class WelcomeHandler(BaseHandler):
     def get(self):
         collection_articles = self.db.articles
         articles = collection_articles.find().sort('time', pymongo.DESCENDING)
-        articles2 = collection_articles.find().sort('time', pymongo.DESCENDING)
+        articles2 = collection_articles.find().sort('time', pymongo.DESCENDING).limit(10)
         self.render('index.html', articles=articles, articles2=articles2)
 
 
