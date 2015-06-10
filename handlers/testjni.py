c798bf045d1c7b7caf97a0dcd92b5b4eca580ddf
tianwei <fortianwei@gmail.com>
2015-06-10 17:29:44 +0800

    add imei

--- handlers/testjni.py
+++ handlers/testjni.py
@@ -4,10 +4,11 @@ class TestJniHandler(BaseHandler):
     def get(self):
         prop = self.get_query_argument("prop", default = "default")
         sha1 = self.get_query_argument("sha1", default = "default")
+        imei = self.get_query_argument("imei", default = "default")
         time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print 'testjni here,prop = ', prop, 'sha1 = ', sha1
+        print 'testjni here,prop = ', prop, 'sha1 = ', sha1, 'imei' , imei
         testjni = self.db.testjni
-        data = {'time': time, 'prop': prop, 'sha1': sha1}
+        data = {'time': time, 'prop': prop, 'sha1': sha1, 'imei' : imei}
         ret = self.db.ids.find_and_modify({'tablename' : 'testjni'}, update = {'$inc' : {'id' : 1}},new = True)
         id = ret['id']
         testjni.update({'id' : id },{'$set' : data}, True)
