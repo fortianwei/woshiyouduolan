2674cab6c402ac890e423fe97648c028b2bd7b72
fortianwei <fortianwei@gmail.com>
2016-01-25 18:18:38 +0800

    改进发布脚本，改掉说明

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-ps -ef|grep -P 'python app.py'|awk '{print $2}' |xargs  kill -9 & git pull origin master && nohup python app.py &
+ps -ef|grep  'python app.py'|awk '{print $2}' |xargs  kill -9 & git pull origin master && nohup python app.py &
