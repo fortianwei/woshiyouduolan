ccf8996cf625feedaed59b1937f04a2c3e8b5d39
tianwei <wei.tian@dianjoy.com>
2016-04-28 14:07:57 +0800

    init bug fix

--- handlers/welcome.py
+++ handlers/welcome.py
@@ -7,7 +7,7 @@ import tornado.gen
 class WelcomeHandler(BaseHandler):
 
     def __init__(self, application, request, **kwargs):
-        super(WelcomeHandler, self).__init__(self, application, request, **kwargs)
+        super(WelcomeHandler, self).__init__(application, request, **kwargs)
         self.page_size = 6
         self.side_articles_num = 8
 
