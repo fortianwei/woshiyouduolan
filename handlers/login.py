1be4535d18a12761b2d0310ef681c4e67804dcd9
fortianwei <fortianwei@gmail.com>
2017-02-20 17:30:53 +0800

    login redirect ok

--- handlers/login.py
+++ handlers/login.py
@@ -3,21 +3,26 @@
 from base import BaseHandler
 import utils
 
+from tornado import gen
 
 class LoginHandler(BaseHandler):
     def get(self):
-        self.render("login.html")
+        next_page = self.get_argument('next', '/')
+        self.render("login.html", next=next_page)
 
+    @gen.coroutine
     def post(self, *args, **kwargs):
         username = self.get_argument('username')
         password = self.get_argument('password')
         password = utils.getBase64EncodedMD5String(password)
         print username, password
-        user = self.db.users.find({'username': username,'password': password})
-        if user.count() > 0:
+        user = self.db.users.find({'username': username, 'password': password})
+        count = yield user.count()
+        if count > 0:
             print 'Check user success.'
             self.set_secure_cookie('current_user', unicode(username))
+            print 'next is ', self.get_argument("next", "/")
             self.redirect(self.get_argument("next", "/"))
         else:
             print 'Check user failed.'
-            pass
+            self.redirect('/')
