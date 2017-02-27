b5bc04bf79471f85027290717fa69698a772c134
fortianwei <fortianwei@gmail.com>
2017-02-27 11:03:50 +0800

    merge multi timelines with date

--- handlers/timeline.py
+++ handlers/timeline.py
@@ -2,14 +2,23 @@
 
 from base import BaseHandler
 from tornado import gen
-class TimelineHandler(BaseHandler):
+from collections import OrderedDict
 
+
+class TimelineHandler(BaseHandler):
     @gen.coroutine
     def get(self):
-
         timelines = self.db.timelines
         pp = yield timelines.find().sort('time', -1).to_list(10000)
-        self.render('timeline.html', pp=pp)
+        new_pp = OrderedDict()
+        for timeline in pp:
+            time_day = timeline['time'][:10]
+            if time_day in new_pp:
+                new_pp[time_day].append(timeline)
+            else:
+                new_pp[time_day] = [timeline]
+
+        self.render('timeline.html', pp=new_pp)
 
     def post(self):
         pass
